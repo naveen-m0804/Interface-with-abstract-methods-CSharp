1147c77f4100ba200afdbd1efbbbacf4a8515dac
Naveen M <117974950+naveen-m0804@users.noreply.github.com>
2024-05-09 09:30:46 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Transactions;
+public interface Bank
+{
+    void deposit();
+    void withdraw();
+}
+public class Program:Bank
+{
+    public int amount,balance=5000;
+    public Program()
+    {
+        Console.WriteLine("Enter your Choice:\n1.Deposit\n2.WithDraw");
+        int opt;
+        opt = Convert.ToInt32(Console.ReadLine());
+        if (opt == 1)
+            deposit();
+        else if (opt == 2)
+            withdraw();
+        else
+        {
+            Console.WriteLine("Enter a valid input");
+        }
+    }
+    public void deposit()
+    {
+        Console.WriteLine("Enter the amount to deposit");
+        amount = Convert.ToInt32(Console.ReadLine());
+        balance += amount;
+        Console.WriteLine("The Balance is " + balance);
+    }
+    public void withdraw()
+    {
+        Console.WriteLine("Enter the amount to withdraw");
+        amount = Convert.ToInt32(Console.ReadLine());
+        balance -= amount;
+        Console.WriteLine("The Balance is " + balance);
+    }
+}
+public class Exp9
+{
+    public static void Main(string[] args)
+    {
+        Program n = new Program();
+    }
+}
